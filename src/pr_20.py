7f0a54d2290058aa84d5f653552420504eb05662
Manav <mahanmanav@gmail.com>
2013-03-20 23:43:21 +0530

    pr20
    Solution to problem 20

---
+++ src/pr_20.py
@@ -0,0 +1,14 @@
+'''
+n! means n  (n  1)  ...  3  2  1
+
+For example, 10! = 10  9  ...  3  2  1 = 3628800,
+and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+Find the sum of the digits in the number 100!
+'''
+
+fact = 1
+for i in range(1, 101):
+    fact = fact * i
+    
+print(sum(int(i) for i in str(fact)))
